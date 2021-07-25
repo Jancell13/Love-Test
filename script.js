741021249b809911865de975c6d672d0fe3821ee
Jancell <71906690+Jancell13@users.noreply.github.com>
2021-07-24 23:38:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -9,48 +9,68 @@ function n(a) {
     a = n3.length / 2;
     return a;
   } else if (n3.length > 10 && n3.length % 2 === 1) {
-    a = (n3.length + 1) / 2;
+    a = n3.length / 2;
     return a;
   } else {
     return a;
   }
 }
-function frases(options) {
-  if (n() === 1) {
-    options = "15%";
-  } else if (n() === 2) {
-    options = "27%";
-  } else if (n() === 3) {
-    options = "93%";
-  } else if (n() === 4) {
-    options = "1%";
-  } else if (n() === 5) {
+function sentence(options) {
+  if (n() === 5) {
     options = "75%";
+  } else if (n() > 5 && n() < 6) {
+    options = "15%";
   } else if (n() === 6) {
     options = "9999%";
+  } else if (n() > 6 && n() < 7) {
+    options = "27%";
   } else if (n() === 7) {
     options = "53%";
+  } else if (n() > 7 && n() < 8) {
+    options = "93%";
   } else if (n() === 8) {
     options = "42%";
+  } else if (n() > 8 && n() < 9) {
+    options = "1%";
   } else if (n() === 9) {
     options = "69%";
-  } else {
+  } else if (n() === 10) {
     options = "38%";
   }
   return options;
 }
 
-btn.addEventListener("click", () => {
+function activeLoader() {
+  loader = document.getElementById("loader");
+  result.innerText = "";
 
-  console.log(n());
-  if (!n1.value.length == 0 && !n2.value.length == 0) {
+  setTimeout(function () {
+    result.style.display = "none";
+    loader.style.display = "block";
+  }, 0);
+}
+function desactiveLoader() {
+  loader = document.getElementById("loader");
+  result.innerText = sentence() + " de compatibilidad";
+  setTimeout(function () {
+    loader.style.display = "none";
+    result.style.display = "block";
+  }, 3500);
+}
 
-    if(n1.value.toLowerCase() == "jancell" || n2.value.toLowerCase() == "jancell"){
+function clic() {
+  if (n1.value.length > 0 && n2.value.length > 0) {
+    if (
+      n1.value.toLowerCase() == "jancell" ||
+      n2.value.toLowerCase() == "jancell"
+    ) {
       alert("Que haces loca.\n¿Por que pones mi nombre?");
-    }else{
-      result.innerText = frases() + " de compatibilidad";
+      /* result.innerText = "9999999%" + " de compatibilidad" */
+    } else {
+      activeLoader();
+      desactiveLoader();
     }
-  }else{
-    alert("Llena los cuadros de texto.");
+  } else {
+    alert("Escribe los nombres.");
   }
-});
+}
